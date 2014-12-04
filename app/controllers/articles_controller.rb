fc9e73321e2176b89be4db1de0d01d2c446239bb
Sebastian Zapata Mardini <sezama03@hotmail.com>
2014-12-04 10:01:37 -0500

    Added button edit when the app render the show view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -28,6 +28,7 @@ class ArticlesController < ApplicationController
 	def show
   		@article = Article.find(params[:id])
   		@comments = @article.comments
+  		@mostrar_editar = true
 	end
 
 	def update
