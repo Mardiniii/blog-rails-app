7e72b53249be96bfb4096ce8c566fcf12edba322
Sebastian Zapata Mardini <sezama03@hotmail.com>
2014-12-05 16:09:53 -0500

    Changed column name commenter to user_id and fixing edit link when show an article

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,7 @@ class CommentsController < ApplicationController
 	def create
 		@article = Article.find(params[:article_id])
 		@comment = @article.comments.build(comment_params)
-		
+		@comment.user = current_user
 		@comment.save
 		redirect_to article_path(@article, anchor: "comment_#{@comment.id}")
 	end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141205203746) do
+ActiveRecord::Schema.define(version: 20141205205231) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 20141205203746) do
   end
 
   create_table "comments", force: true do |t|
-    t.integer  "commenter",  limit: 255
+    t.integer  "user_id",    limit: 255
     t.text     "body"
     t.integer  "article_id"
     t.datetime "created_at"
