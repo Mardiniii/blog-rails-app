0a31de0038ba56a64a398e85929282fa6d16dc48
Sebastian Zapata Mardini <sezama03@hotmail.com>
2014-12-05 15:39:39 -0500

    Deleted commenter field from comment form view, added a migration to change the data type of commenter to integer

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,9 @@ class CommentsController < ApplicationController
 
 	def create
 		@article = Article.find(params[:article_id])
-		@comment = @article.comments.create(comment_params)
+		@comment = @article.comments.build(comment_params)
+		
+		@comment.save
 		redirect_to article_path(@article, anchor: "comment_#{@comment.id}")
 	end
 	
