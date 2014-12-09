366a8087fd6c410862721514bcd2230a578f6469
Sebastian Zapata Mardini <sezama03@hotmail.com>
2014-12-09 15:14:27 -0500

    Set value nill to author column before to run migration

--- db/migrate/20141205211325_change_column_type_author.rb
+++ db/migrate/20141205211325_change_column_type_author.rb
@@ -1,5 +1,6 @@
 class ChangeColumnTypeAuthor < ActiveRecord::Migration
   def change
+  	Article.update_all(author: nil)
   	change_column :articles, :author, 'integer USING CAST(author AS integer)'
   end
 end
